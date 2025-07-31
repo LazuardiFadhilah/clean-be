9552fdbd7af408707c5364c5ad2f5009bf3c8285
Lazuardi Fadhilah <Lazuardifadhillah@gmail.com>
2025-08-01 00:24:53 +0700

    Refactor CORS configuration to use specific options for improved security and flexibility

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors())
+
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN || 'http://localhost:3001', // Default to localhost if not set
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
-// const corsOptions = {
-//   origin: process.env.CORS_ORIGIN || 'http://localhost:3001', // Default to localhost if not set
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// };
-
-// app.use(cors(corsOptions));
 
 
 app.use('/api/auth', require('./routes/authRoutes'));
