f1e834d8cf96e51d211690a98a659b1a55f146e4
Lazuardi Fadhilah <Lazuardifadhillah@gmail.com>
2025-08-01 23:28:00 +0700

    Refactor CORS configuration to use a single origin and remove commented-out code

--- app.js
+++ app.js
@@ -1,36 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
-console.log('CORS_ORIGIN dari variabel lingkungan:', process.env.CORS_ORIGIN);
+console.log("CORS_ORIGIN dari variabel lingkungan:", process.env.CORS_ORIGIN);
 
-const allowedOrigins = [
-    process.env.CORS_ORIGIN ,
-    'http://localhost:3001',
-];
+app.use(
+  cors({
+    origin: "http://localhost:3001",
 
-app.use(cors({
-    // origin: function (origin, callback){
-    //     if(!origin) return callback(null, true);
-    //     if(allowedOrigins.includes(origin)) {
-    //         return callback(null, true);
-    //     } else {
-    //         return callback(new Error('CORS policy: Origin not allowed'), false);
-    //     }
-    // },
-    origin:'*',
-  credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}))
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 
-
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/bookings', require('./routes/bookingRoutes'));
-app.use('/api/billings', require('./routes/billingRoutes'));
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/bookings", require("./routes/bookingRoutes"));
+app.use("/api/billings", require("./routes/billingRoutes"));
 
 module.exports = app;
